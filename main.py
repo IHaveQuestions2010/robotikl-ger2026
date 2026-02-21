55cf6d2c0412f76b89df1333b121167218067abf
Astynax0 <astyanax.xli@gmail.com>
2026-02-21 17:10:47 +0100

    Refactor line following logic and adjust parameters

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import runloop
 canStart = False
 motor_pair.pair(motor_pair.PAIR_1, port.F, port.D)
 
-
 # CONFIG
 reflectivnessTarget = 50
 sensorPort = port.C
@@ -15,31 +14,23 @@ pair = motor_pair.PAIR_1
 
 
 
-async def sensorFollowLine():
-    integral = 0
-    last_error = 0
 
-    Kp = 1.8
-    Ki = 0.01
-    Kd = 0.8
+
+async def followLine():
     while canStart == False:
         await runloop.sleep_ms(50)
     while True:
-        # https://issssse.github.io/spikeguide/#lls-help-python-examples-pid-control
+        # https://issssse.github.io/spikeguide/#lls-help-python-examples-p-control
         reflectivnessReading = color_sensor.reflection(sensorPort)
         reflectivnessError = reflectivnessTarget - reflectivnessReading
-
-        P = Kp * reflectivnessError
-
-        integral += reflectivnessError
-        I = Ki * integral
-
-        derivative  = reflectivnessError - last_error
-        D = Kd * derivative
-
-        last_error = reflectivnessError
-        steering = int(P + I + D)
+        steering = int(reflectivnessError * 0.8)
         motor.run_for_degrees(sensorMotorPort, -steering, 90)
+        if motor.absolute_position(sensorMotorPort) >= 90:
+            motor.stop(sensorMotorPort)
+            while color_sensor.reflection(sensorPort) >= 97:
+                motor.run_for_degrees(sensorMotorPort, -1, 90)
+                if motor.absolute_position(sensorMotorPort) <= -90:
+                    motor_pair.move_tank_for_time(motor_pair.PAIR_1, 110, 110, 2000)
 
         await runloop.sleep_ms(50)
 
@@ -51,13 +42,17 @@ async def main():
     while True:
         if color_sensor.color(sensorPort) == color.RED:
             print("WIN")
+            motor_pair.stop(motor_pair.PAIR_1)
+            motor.stop(sensorMotorPort)
             break
-        elif color_sensor.reflection(sensorPort) <= 90:
+        elif color_sensor.reflection(sensorPort) <= 97:
             # When sensorAngle increases the robot will turn more cancelling it out so it follows the line
             sensorAngle = motor.absolute_position(sensorMotorPort)
-            motor_pair.move(pair, int(2.5*sensorAngle), velocity=-120)
+            motor_pair.move(pair, int(2.5*sensorAngle), velocity=-110)
+
         else:
             motor_pair.stop(pair)
+            
         await runloop.sleep_ms(50)
 
 runloop.run(main(), followLine())
