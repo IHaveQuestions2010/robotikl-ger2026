c4fba6d59e59e2d38c43b2629a78d46dd584dcd0
IHaveQuestions <121615203+IHaveQuestions2010@users.noreply.github.com>
2026-02-22 10:17:03 +0100

    Refactor line following logic and variable names

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import motor, motor_pair, color_sensor,color
 import runloop
 
 # INIT
-pauseFollowLine = True
+runSensorFollowLine = False
 motor_pair.pair(motor_pair.PAIR_1, port.F, port.D)
 
 
@@ -13,6 +13,17 @@ sensorPort = port.C
 sensorMotorPort = port.A
 pair = motor_pair.PAIR_1
 
+"""
+async def noLineFound():
+        if motor.absolute_position(sensorMotorPort) >= 90:
+            motor.stop(sensorMotorPort)
+            while color_sensor.reflection(sensorPort) >= 91:
+                motor.run_for_degrees(sensorMotorPort, -1, 90)
+                if motor.absolute_position(sensorMotorPort) <= -90:
+                    await motor.run_to_absolute_position(sensorMotorPort, 0, 90, direction=motor.SHORTEST_PATH)
+                    await motor_pair.move_tank_for_time(pair, -110, -110, 2000)
+                    break
+"""
 
 async def sensorFollowLine():
     integral = 0
@@ -21,9 +32,10 @@ async def sensorFollowLine():
     Kp = 1.8
     Ki = 0.01
     Kd = 0.8
-    while pauseFollowLine == True:
-        await runloop.sleep_ms(50)
+
     while True:
+        while runSensorFollowLine == False:
+            await runloop.sleep_ms(50)
         # https://issssse.github.io/spikeguide/#lls-help-python-examples-pid-control
         reflectivnessReading = color_sensor.reflection(sensorPort)
         reflectivnessError = reflectivnessTarget - reflectivnessReading
@@ -45,38 +57,31 @@ async def sensorFollowLine():
 async def main():
     await motor.run_to_absolute_position(sensorMotorPort, 0, 180, direction=motor.SHORTEST_PATH)
     # Allows followLine() to run only after the sensor is centered
-    global pauseFollowLine
-    pauseFollowLine = False
+    global runSensorFollowLine
+    runSensorFollowLine = True
     while True:
         if color_sensor.color(sensorPort) == color.RED:
             print("WIN")
-            pauseFollowLine = True
             motor_pair.stop(pair)
             motor.stop(sensorMotorPort)
             break
-        # Triggers when the line is detected
         elif color_sensor.reflection(sensorPort) <= 90:
-            pauseFollowLine = False
             # When sensorAngle increases the robot will turn more cancelling it out so it follows the line
             sensorAngle = motor.absolute_position(sensorMotorPort)
             motor_pair.move(pair, int(2.5*sensorAngle), velocity=-120)
-        # Triggers when the robot loses the line
         else:
-            # Intended logic
-            # 1. Stop moving forward
-            # 2. Try to find the line on the left side
-                # 2a. If the line is found, follow it
-                # 2b. If the line was not found try to find it on the right side
-            # 3. Move forward for two seconds or until the line is found
-
-            motor.stop(pair)
+            hasCheckedLeft = False
+            motor_pair.stop(pair)
             if motor.absolute_position(sensorMotorPort) >= 90:
-                pauseFollowLine = True
-                motor.stop(sensorMotorPort)
-                motor.run_to_absolute_position(sensorMotorPort, -90, 90)
-            if motor.absolute_position(sensorMotorPort) <= -90:
-                await motor.run_to_absolute_position(sensorMotorPort, 0, 90, direction=motor.SHORTEST_PATH)
-                await motor_pair.move_tank_for_time(pair, -110, -110, 2000)
+                if hasCheckedLeft == True:
+                    await motor.run_to_absolute_position(sensorMotorPort, 0, 90, direction=motor.SHORTEST_PATH)
+                    await motor_pair.move_tank_for_time(pair, -110, -110, 2000)
+                    hasCheckedLeft = False
+                else:
+                    runSensorFollowLine = False
+                    await motor.run_to_absolute_position(sensorMotorPort, 90, 90, direction=motor.SHORTEST_PATH)
+                    runSensorFollowLine = True
+                    hasCheckedLeft = True
         await runloop.sleep_ms(50)
 
 runloop.run(main(), sensorFollowLine())
